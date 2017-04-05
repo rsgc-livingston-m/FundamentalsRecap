6087fdc2755088811cc39986c0cf49ea3094853f
Student <Max@macbook-pro-423-livingston.local>
2017-04-05 13:11:29 -0400

    I was able to complete the second challenge by rotating the image by 45 degrees.

--- Recap.playground/Pages/Translation and Rotation.xcplaygroundpage/Contents.swift
+++ Recap.playground/Pages/Translation and Rotation.xcplaygroundpage/Contents.swift
@@ -34,7 +34,7 @@ let canvas = Canvas(width: 400, height: 400)
 // Draw slightly thicker lines
 canvas.defaultLineWidth = 4
 canvas.translate(byX: 200, byY: 200)
-
+canvas.rotate(by: 45)
 // Draw the vertical axis and label it
 canvas.drawLine(fromX: -200, fromY: 0, toX: 200, toY: 0)
 canvas.drawText(message: "y", size: 24, x: 10, y: 175)
